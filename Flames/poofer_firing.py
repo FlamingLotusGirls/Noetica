8b3e6a84d4f01e46728b6e7ae0319df2957ce136
Josh Karli <josh.karli@gmail.com>
2017-07-05 22:23:52 -0700

    test push

---
+++ Flames/poofer_firing.py
@@ -0,0 +1,39 @@
+import 
+
+
+# This file is designed to assemble and send the commands to the poofer controller boards.
+# The bang protocol used for comms to communicate with poofer controller board.
+# Bang protocol defined here: http://flg.waywardengineer.com/index.php?title=Bang_(!)_Protocol
+
+# variables (changeable)
+
+
+### parameters - do not change ###
+#determined by the switching speed of the DMI-SH-112L relay on the poofer controller board
+minimumFiringDuration=20
+timeWaitForNextPoofAfterAPoof=20
+
+#proof of concept: fire a single poofer
+
+
+
+#create bang code for a single channel
+
+
+
+#create a sequence of bang commands
+
+
+
+#send a sequence of bang commands to 
+##we need to figure out what the upper limits are for firing off commands
+##(minimum firing duration) 
+
+
+
+
+
+
+
+
+
