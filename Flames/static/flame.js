2994e265cc2bc384b348e5c6f622c8ad9c249847
Thom Blake <tblake@pagerduty.com>
2017-08-15 18:52:20 -0700

    Replace name with ip

--- Flames/static/flame.js
+++ Flames/static/flame.js
@@ -1,6 +1,6 @@
 $(function ($) {
   // Constants
-  var flameHost = 'noetica-flames.local:5000'
+  var flameHost = '192.168.7.101:5000'
   var pollingInterval = 1000 // in milliseconds
   var prefixes = {
     hydraulics: 'hydraulics-attract',
